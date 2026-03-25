bfcef4ff349af40900de21924b50a0a550a1e64d
GaoGaoCE <Hai.Dao@ssit.com.vn>
2026-03-25 15:24:04 +0700

    add thêm axisrobotics

--- Open-Multi-Airdrop-Browser/Multitab.bat
+++ Open-Multi-Airdrop-Browser/Multitab.bat
@@ -11,3 +11,4 @@ start chrome https://portal.stormrae.ai?ref=gaogaocrypto
 start chrome https://www.neftit.xyz/
 start chrome https://hub.konnex.world/points?referral_code=PE0OOO1X
 start chrome https://cedomis.xyz/dashboard
+start chrome https://hub.axisrobotics.ai/?tab=hub
