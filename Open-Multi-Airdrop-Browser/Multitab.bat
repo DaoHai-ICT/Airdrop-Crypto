f16e1cda2318a45543be3e25d3165ad73a5a9566
GaoGaoCE <Hai.Dao@ssit.com.vn>
2026-03-27 11:33:20 +0700

    xoá degen

--- Open-Multi-Airdrop-Browser/Multitab.bat
+++ Open-Multi-Airdrop-Browser/Multitab.bat
@@ -6,7 +6,6 @@ start chrome https://hub.playai.network/missions
 start chrome https://waitlist.kindredlabs.ai/dashboard
 start chrome https://rewards.dtelecom.org/reward?referral_code=GGCRYPTO
 start chrome https://app.decibel.trade/trade
-start chrome https://dream.degen.tips/dashboard
 start chrome https://portal.stormrae.ai?ref=gaogaocrypto
 start chrome https://www.neftit.xyz/
 start chrome https://hub.konnex.world/points?referral_code=PE0OOO1X
